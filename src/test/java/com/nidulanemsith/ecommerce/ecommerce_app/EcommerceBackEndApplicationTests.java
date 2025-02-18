cbba315992b3a91c87ee8988b2f8fdd21e817df5
nemsi <nemsithbaduge215@gmail.com>
2025-02-18 21:47:16 +0530

    initialize Spring Boot e-commerce application with Maven setup

---
+++ src/test/java/com/nidulanemsith/ecommerce/ecommerce_app/EcommerceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidulanemsith.ecommerce.ecommerce_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
