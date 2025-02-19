2f21cb1e079c45c5415e2e04b56befb09bc2fcb6
nemsi <nemsithbaduge215@gmail.com>
2025-02-19 18:18:04 +0530

    add controllers and security dependency for e-commerce application

---
+++ src/main/java/com/nidulanemsith/ecommerce/ecommerce_app/controller/NoticesController.java
@@ -0,0 +1,12 @@
+package com.nidulanemsith.ecommerce.ecommerce_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Ecommerce Application";
+    }
+}
