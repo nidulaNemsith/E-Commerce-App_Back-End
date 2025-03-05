e6e24d2d5082c069a335cef616078ae58a350fd2
nemsi <nemsithbaduge215@gmail.com>
2025-03-05 16:16:24 +0530

    add password encoder and compromised password checker beans to security configuration

--- src/main/java/com/nidulanemsith/ecommerce/ecommerce_app/config/ProjectSecurityConfig.java
+++ src/main/java/com/nidulanemsith/ecommerce/ecommerce_app/config/ProjectSecurityConfig.java
@@ -2,8 +2,12 @@ package com.nidulanemsith.ecommerce.ecommerce_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,8 +24,14 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
-
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
 
 }
